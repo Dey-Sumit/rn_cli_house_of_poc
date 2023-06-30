3212b924a36d147e776d00e2d8a5a639a1bf64b1
Sumit Dey <sumitdey@Sumits-MacBook-Pro.local>
2023-07-01 00:47:58 +0530

    feat: added qr-scanner and patch the react-native-camera package

---
+++ components/1-QrCodeScanner.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import {Linking, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {BarCodeReadEvent, RNCamera} from 'react-native-camera';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+
+const QrCodeScanner = () => {
+  const onSuccess = (e: BarCodeReadEvent) => {
+    Linking.openURL(e.data).catch(err =>
+      console.error('An error occurred', err),
+    );
+  };
+  return (
+    <View>
+      <QRCodeScanner
+        onRead={onSuccess}
+        flashMode={RNCamera.Constants.FlashMode.torch}
+        topContent={
+          <Text style={styles.centerText}>
+            Go to{' '}
+            <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
+            your computer and scan the QR code.
+          </Text>
+        }
+        bottomContent={
+          <TouchableOpacity style={styles.buttonTouchable}>
+            <Text style={styles.buttonText}>OK. Got it!</Text>
+          </TouchableOpacity>
+        }
+      />
+    </View>
+  );
+};
+
+export default QrCodeScanner;
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+  buttonTouchable: {
+    padding: 16,
+  },
+});
